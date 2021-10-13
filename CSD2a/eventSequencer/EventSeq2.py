ae601c5dd86c9c1766520d81ff99635ea40c6903
Rahsaan Bleijs <rahsaan@live.nl>
2021-10-13 20:26:33 +0200

    minor update on formatting

--- CSD2a/eventSequencer/EventSeq2.py
+++ CSD2a/eventSequencer/EventSeq2.py
@@ -5,24 +5,24 @@ nD = 0.25 #noteduration
 kickSample = sa.WaveObject.from_wave_file("kick.wav")
 snareSample = sa.WaveObject.from_wave_file("snare.wav")
 hihatSample = sa.WaveObject.from_wave_file("hihat.wav")
-HHts =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12,13, 14]
-Kts = [0, 4, 7, 8, 12, 15]
-Sts = [2, 6, 10, 14, 15]
+hhTs =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12,13, 14]
+kTs = [0, 4, 7, 8, 12, 15]
+sTs = [2, 6, 10, 14, 15]
 event = []
 
 
 def createEventList():
-    for hat in HHts:
+    for hat in hhTs:
         event.append({
             'timestamp' : hat,
             'instrument' : 'hihat'
         })
-    for kick in Kts:
+    for kick in kTs:
         event.append({
             'timestamp' : kick,
             'instrument' : 'kick'
         })
-    for snare in Sts:
+    for snare in sTs:
         event.append({
             'timestamp' : snare,
             'instrument' : 'snare'
