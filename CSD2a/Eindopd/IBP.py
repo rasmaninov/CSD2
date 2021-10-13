0f481baa1f7ddaa2ad8676f727ad301c37be6e4f
Rahsaan Bleijs <rahsaan@live.nl>
2021-10-13 20:27:10 +0200

    added function in separate file "TestModuleAlgo.py", function creates timestamps for 1/3 layers.

--- CSD2a/Eindopd/IBP.py
+++ CSD2a/Eindopd/IBP.py
@@ -1,52 +1,76 @@
 import simpleaudio as sa
 import time
-
+from TestModuleAlgo import split_note
 nD = 0.25 #noteduration
-Tab1Sample = sa.WaveObject.from_wave_file("Tab1.wav")
-Tab2Sample = sa.WaveObject.from_wave_file("Tab2.wav")
-TablongSample = sa.WaveObject.from_wave_file("Tablong.wav")
-event = []
+Tab_1_Sample = sa.WaveObject.from_wave_file("Tab1.wav")
+Tab_2_Sample = sa.WaveObject.from_wave_file("Tab2.wav")
+tab_long_sample = sa.WaveObject.from_wave_file("tablong.wav")
+events = []
 
-t1ts = []
-t2ts = []
-tlts = []
+t1_sequence = [28, 4]
+t1_ts = []
+t2_ts = []
+tl_ts = []
 
-def createEventList():
-    for hat in t1ts:
-        event.append({
+random_range = 12
+random_diminisher = 0.5
+
+for i in range(3):
+    num_splits = 2 ** i
+
+    for j in range(num_splits):
+        index = j * 2
+        split_note(t1_sequence, index, random_range)
+
+    random_range = int(random_range * random_diminisher + 0.5)
+
+print(t1_sequence, "sequens", len(t1_sequence))
+
+current_time = 0 #thanks jochem
+
+for i in range(len(t1_sequence)):
+    t1_ts.append(current_time)
+    current_time = current_time + t1_sequence[i]
+
+print(t1_ts, "timestamps", len(t1_ts))
+
+
+def createeventsList():
+    for hat in t1_ts:
+        events.append({
             'timestamp' : hat,
             'instrument' : 'Tab1'
         })
-    for Tablong in tlts:
-        event.append({
-            'timestamp' : Tablong,
-            'instrument' : 'Tablong'
+    for tab_long in tl_ts:
+        events.append({
+            'timestamp' : tab_long,
+            'instrument' : 'tab_long'
         })
-    for Tab2 in t2ts:
-        event.append({
+    for Tab2 in t2_ts:
+        events.append({
             'timestamp' : Tab2,
             'instrument' : 'Tab2'
         })
 
 
-createEventList()
+createeventsList()
 
-def get_timestamp(event):
-    return event.get('timestamp')
-event.sort(key =get_timestamp)
+def get_timestamp(events):
+    return events.get('timestamp')
+events.sort(key =get_timestamp)
 
-def noteEvent(event):
+def note_events(events):
 
-    if event['instrument'] == 'Tab2':
-        Tab2Sample.play()
+    if events['instrument'] == 'Tab2':
+        Tab_2_Sample.play()
 
-    if event['instrument'] == 'Tablong':
-        TablongSample.play()
+    if events['instrument'] == 'tab_long':
+        tl_ts.play()
 
-    if event['instrument'] == 'Tab1':
-        Tab1Sample.play()
+    if events['instrument'] == 'Tab1':
+        Tab_1_Sample.play()
 
-noteEvent({
+note_events({
     'instrument':'Tab1'
 })
 
@@ -54,26 +78,37 @@ time.sleep(1)
 print("go")
 t0 = time.time()
 
-while len(event):
+while len(events):
     t = time.time() - t0
-    if (len(event)> 0):
-        if (len(event) > 0 and t >= (event[0].get('timestamp')*nD) and event[0].get('instrument') == 'Tab1'):
-            noteEvent({
+    if (len(events)> 0):
+        if (len(events) > 0
+        and t >= (events[0].get('timestamp')*nD)
+        and events[0].get('instrument') == 'Tab1'):
+            note_events({
                 'instrument':'Tab1'
             })
-            event.pop(0)
-        if (len(event) > 0 and t >= (event[0].get('timestamp')*nD) and event[0].get('instrument') == 'Tablong'):
-            noteEvent({
-                'instrument':'Tablong'
+            events.pop(0)
+
+        if (len(events) > 0
+        and t >= (events[0].get('timestamp')*nD)
+        and events[0].get('instrument') == 'tab_long'):
+            note_events({
+                'instrument':'tab_long'
             })
-            event.pop(0)
-        if (len(event) > 0 and t >= (event[0].get('timestamp')*nD) and event[0].get('instrument') == 'Tab2'):
-            noteEvent({
+            events.pop(0)
+
+        if (len(events) > 0
+        and t >= (events[0].get('timestamp')*nD)
+        and events[0].get('instrument') == 'Tab2'):
+            note_events({
                 'instrument':'Tab2'
             })
-            event.pop(0)
+            events.pop(0)
+
         time.sleep(0.01)
     else:
         break
+
 time.sleep(0.5)
+
 print("klaar")
--- CSD2a/Eindopd/Readme.md
+++ CSD2a/Eindopd/Readme.md
@@ -1,16 +1,24 @@
 IBP - Indian Beat Programmer
 
-Deze Beatgenerator is gebaseerd op ritmes uit de Indiase klassieke muziek.
+Deze Beatgenerator is gebaseerd op ritmes en de maatsoorten uit de Indiase klassieke muziek, en opgebouwd uit 3 lagen: low , mid, high.
 
 Tala : de indiase meter
-Adi Tala - 8 beat cycle
+
+Khanda Jati Rupaka Tala - 7  Beat cycle - 2 + 5         = 7
+Tisra Javi Dhruva Tala  - 11 Beat cycle - 3 + 2 + 3 + 3 = 11
+
+De low laag valt in beide beat cycles minstens 1 keer per 'onderverdeling' van de maat. Dus in de Khanda Rupaka minstens 2 keer en in de Tisra Dhruva minstens 4 keer.
+TODO :
+
+
+  kijken welke iig 2 maatsoorten en hoe die onderverdeeld zijn.
+
+  op basis daarvan algo schrijven
 
 
 
-
-
-
-
+  Tablong spelen op basis van timestamps van algoritmisch geselecteerde timestamps uit t1ts
+  functie schrijven op basis van de indiase maatsoorten op basis van de maatsoorten
 
 
 Er zijn 4 lagen (even vragen)
