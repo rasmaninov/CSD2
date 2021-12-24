b8da358539492efb70a1033e29b6f05052edccff
Rahsaan Bleijs <rahsaan@live.nl>
2021-12-24 18:46:24 +0100

    nothing changedlol

--- CSD2b/EINDOPDRACHT&documents/melodygen.cpp
+++ CSD2b/EINDOPDRACHT&documents/melodygen.cpp
@@ -11,6 +11,7 @@ Melody::~Melody(){
 void Melody::MelodyGen(){
   for (unsigned int i = 0; i < 16; i++){
     newPitch = (rand() % 60 + 20)%12 + 48;
+
     notes[i] = newPitch;
     noteCount = noteCount + 1;
   }
