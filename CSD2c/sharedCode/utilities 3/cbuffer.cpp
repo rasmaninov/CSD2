d00ddd7411ba943b5d9037212cc5b43d73f05387
Rahsaan Bleijs <rahsaan@live.nl>
2022-03-13 21:28:39 +0100

    moved extras

---
+++ CSD2c/sharedCode/utilities 3/cbuffer.cpp
@@ -0,0 +1,46 @@
+#include "cbuffer.h"
+#include <cmath>
+// initializer list = values after colon
+Cbuffer::Cbuffer(int size, int numSamplesDelay) :
+  readH(size - numSamplesDelay),
+  writeH(0),
+  size(size),
+  numSamplesDelay(numSamplesDelay)
+{
+  // creating buffer thats sized[size]
+  buffer = new float[size];
+  // initializing every value in buffer array
+  for(int i =0; i < size; i++){
+    buffer[i] = 0;
+  }
+
+}
+
+Cbuffer::~Cbuffer(){
+  // delete dynamic array
+  delete [] buffer;
+}
+
+void Cbuffer::write(float value){
+
+  buffer[writeH++] = value;
+  writeH = wrapH(writeH);
+}
+
+float Cbuffer::read(){
+  float value = buffer[readH++];
+  readH = wrapH(readH);
+  return value;
+}
+
+int Cbuffer::wrapH(int head){
+  if(head >= size) head -= size;
+  return head;
+}
+
+void Cbuffer::setNumSamplesDelay(float delayInSamps){
+  this->numSamplesDelay =  floor(delayInSamps);
+  this->readH = writeH - numSamplesDelay + size;
+  readH = wrapH(readH);
+  std::cout << readH << std::endl;
+}
