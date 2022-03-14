5a4731f919e00551ee53c0db57082ece01611298
Rahsaan Bleijs <rahsaan@live.nl>
2022-03-14 14:14:41 +0100

    working chorus

--- CSD2c/stereo_effect_base.cpp/stereo_main.cpp
+++ CSD2c/stereo_effect_base.cpp/stereo_main.cpp
@@ -1,20 +1,22 @@
 #include "jack_module.h"
-#include "sine.h"
 #include <string>
 #include <math.h>
 #include <thread>
 #include <unistd.h>
 #include <iostream>
 #include "delay.h"
+#include "sine.h"
 
 unsigned long chunksize = 2048;
 JackModule jack;
 float samplerate = 44100;
-Delay delayL(samplerate, 0.99, 0.7, 200); //samplerate,moddepth,feedback,delayms
-Delay delayR(samplerate, 0.99, 0.7, 400);
+Delay delayL(samplerate, 1, 0, 3); //samplerate,moddepth,feedback,delayms
+Delay delayR(samplerate, 1, 0, 7);
 bool running = true;
 float outbufL;
 float outbufR;
+
+Sine sine(10, samplerate);
 int main(int argc, char **argv){
 
   jack.init(argv[0]);
@@ -31,10 +33,14 @@ int main(int argc, char **argv){
     for(unsigned int x=0; x<chunksize; x++)
     {
       // ... your algorithm here
+      float delaytime;
+      delaytime = (sine.genNextSample() + 5) * 2;
+      delayR.delayMS(delaytime);
+
       delayL.processFrame(inbuffer[x], outbufL);
       delayR.processFrame(inbuffer[x], outbufR);
-      outbuffer[2*x]= outbufL; //left channel?
-      outbuffer[2*x+1]= outbufR; //right channel?
+      outbuffer[2*x]= outbufL * 0.2; //left channel?
+      outbuffer[2*x+1]= outbufR * 0.2; //right channel?
     }
     jack.writeSamples(outbuffer,chunksize*2);
   } while(running);
